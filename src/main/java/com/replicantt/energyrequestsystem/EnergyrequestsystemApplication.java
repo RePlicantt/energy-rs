ba70536e1aa6a5a87794c654d0dab5a7efd05876
RePlicantt <vladlen12121@gmail.com>
2025-04-09 19:03:00 +0300

    first commit

---
+++ src/main/java/com/replicantt/energyrequestsystem/EnergyrequestsystemApplication.java
@@ -0,0 +1,13 @@
+package com.replicantt.energyrequestsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyrequestsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyrequestsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replicantt/energyrequestsystem/EnergyrequestsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replicantt.energyrequestsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyrequestsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
