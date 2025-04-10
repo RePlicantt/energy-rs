87871ed876832aa48b65bd7353079f99c85af75e
RePlicantt <vladlen12121@gmail.com>
2025-04-10 22:02:27 +0300

    Add RequestController for handling requests and RequestService for business logic fix: name of packages in repository classes

---
+++ src/main/java/com/replicantt/energyrs/controller/RequestController.java
@@ -0,0 +1,43 @@
+package com.replicantt.energyrs.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.replicantt.energyrs.repository.Request;
+import com.replicantt.energyrs.service.RequestService;
+
+@RestController
+public class RequestController {
+    private final RequestService requestService;
+    public RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @GetMapping("/requests")
+    public List<Request> findAll() {
+        return requestService.getAllRequests();
+    }
+    @PostMapping("/requests")
+    public Request addRequest(Request request) {
+        return requestService.addRequest(request);
+    }
+    @DeleteMapping("/requests/{id}")
+    public void deleteRequest(Long id) {
+        requestService.deleteRequest(id);
+    }
+
+    @PutMapping("/requests/{id}")
+    public void updateRequest(
+        @RequestParam(required = true) String id,
+        @RequestParam(required = true) String type,
+        @RequestParam(required = true) String action
+    ) {
+        requestService.updateRequest(id, type, action);
+    }
+}
